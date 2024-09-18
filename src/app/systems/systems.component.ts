f64d7d262aa32c43f81e8273a765c0405052bc4c
gvfbossa <gvfbossa@hotmail.com>
2024-09-18 11:45:25 -0300

    Adjusted system slogan to fit smartphone screens

--- src/app/systems/systems.component.ts
+++ src/app/systems/systems.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class SystemsComponent implements OnInit, OnDestroy {
   words: string[] = [
-    'Transformamos Complexidade em Simplicidade',
+    'Transformar o Complexo em Simplicidade',
   ];
   currentWordIndex: number = 0;
   typingTimeout: any;
