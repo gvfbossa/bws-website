8a1a6ac6189c3438ac84dce4c8aaf2989fb08f03
Guilherme Bossa <gvfbossa@hotmail.com>
2024-11-10 12:55:43 -0300

    Added Google and Firebase Analytics

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "AIzaSyDGffsqTL9VIFt7t79yarxX3umoTxu2T_I",
+      authDomain: "bws-website-94138.firebaseapp.com",
+      projectId: "bws-website-94138",
+      storageBucket: "bws-website-94138.firebasestorage.app",
+      messagingSenderId: "91918276628",
+      appId: "1:91918276628:web:fff142621bc7e762f36f01",
+      measurementId: "G-GV4Z5R4RVH"
+    }
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDGffsqTL9VIFt7t79yarxX3umoTxu2T_I",
+    authDomain: "bws-website-94138.firebaseapp.com",
+    projectId: "bws-website-94138",
+    storageBucket: "bws-website-94138.firebasestorage.app",
+    messagingSenderId: "91918276628",
+    appId: "1:91918276628:web:fff142621bc7e762f36f01",
+    measurementId: "G-GV4Z5R4RVH"
+  }
+};
