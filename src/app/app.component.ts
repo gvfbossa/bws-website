42480435fd6b02d2085f76de20994870dabaae50
Guilherme Bossa <gvfbossa@hotmail.com>
2025-11-03 19:48:02 -0300

    adicao cliente ifi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
-// Garante que o TypeScript reconheça o gtag
-declare global { interface Window { gtag?: (...args: any[]) => void } }
+declare global { interface Window { dataLayer: any[]; gtag: (...args: any[]) => void; } }
 
 @Component({
   selector: 'app-root',
@@ -14,18 +13,29 @@ export class AppComponent {
   title = 'bws-website';
 
   constructor(private router: Router) {
+
+    const gtagScript = document.createElement('script');
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-GV4Z5R4RVH';
+    gtagScript.async = true;
+    document.head.appendChild(gtagScript);
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function(){ window.dataLayer.push(arguments); };
+    window.gtag('js', new Date());
+    window.gtag('config', 'G-GV4Z5R4RVH', {
+      cookie_flags: 'SameSite=None;Secure',
+      cookie_domain: 'bossawebsolutions.com.br'
+    });
+
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: any) => {
-
-      if (window.gtag) {
-        window.gtag('event', 'page_view', {
-          page_path: event.urlAfterRedirects,
-          page_title: document.title,
-          page_location: window.location.href
-        });
-      }
-
+      window.gtag('event', 'page_view', {
+        page_path: event.urlAfterRedirects,
+        page_title: document.title,
+        page_location: window.location.href
+      });
     });
+
   }
 }
