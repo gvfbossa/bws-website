ca82fbe77c63485a1219bae0259bf193449ed449
Guilherme Bossa <gvfbossa@hotmail.com>
2025-12-10 14:16:29 -0300

    add portfolio recebe facil

--- src/app/service/portfolio.service.ts
+++ src/app/service/portfolio.service.ts
@@ -10,7 +10,14 @@ export class PortfolioService {
       description: 'Um portal de notícias completo e gerenciável com anúncios',
       images: ['assets/images/portfolio-portal1.png', 'assets/images/portfolio-portal2.png', 'assets/images/portfolio-portal3.png', 'assets/images/portfolio-portal4.png'],
       url: 'https://portfolio.portal-noticias.bossawebsolutions.com.br/',
-    },/** 
+    },
+    {
+      title: 'Recebe Fácil',
+      description: 'Cobranças Automáticas. Simples, Rápidas e Sem Dor de Cabeça.',
+      images: ['assets/images/portfolio-recebefacil1.png', 'assets/images/portfolio-recebefacil2.png', 'assets/images/portfolio-recebefacil3.png', 'assets/images/portfolio-recebefacil4.png'],
+      url: 'https://recebefacil.bossawebsolutions.com.br/',
+    },
+    /** 
     {
       title: 'Loja Virtual',
       description: 'Loja virtual para você entrar de vez no mundo online e expandir seu negócio',
